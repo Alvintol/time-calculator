e191052dde800d6f5f531f6ad1dee546ef1c01d3
Alvin <alvin.tol@hotmail.com>
2022-09-07 12:31:13 -0600

    updates initial day argument value

--- timeCalculator.py
+++ timeCalculator.py
@@ -1,9 +1,9 @@
 import re
 
-def add_time(startTime, duration, day = False) :
+def add_time(startTime, duration, day = '') :
   print('STARTIME:', startTime)
   print('DURATION:', duration)  
-  if day : print('DAY OF THE WEEK:', day)
+  if day != '' : print('DAY OF THE WEEK:', day)
   
   amPm = f' {startTime[-2:]}'
   print('AM/PM:', amPm)
@@ -21,19 +21,19 @@ def add_time(startTime, duration, day = False) :
     print(''.join(splitTime))
   
 add_time("3:00 PM", "3:10")
-# Should Returns: 6:10 PM
+# Should Return: 6:10 PM
 
-# add_time("11:30 AM", "2:32", "Monday")
-# # Should Returns: 2:02 PM, Monday
+add_time("11:30 AM", "2:32", "Monday")
+# Should Return: 2:02 PM, Monday
 
 # add_time("11:43 AM", "00:20")
-# # Should Returns: 12:03 PM
+# Should Return: 12:03 PM
 
 # add_time("10:10 PM", "3:30")
-# # Should Returns: 1:40 AM (next day)
+# Should Return: 1:40 AM (next day)
 
 # add_time("11:43 PM", "24:20", "tueSday")
-# # Should Returns: 12:03 AM, Thursday (2 days later)
+# Should Return: 12:03 AM, Thursday (2 days later)
 
 # add_time("6:30 PM", "205:12")
-# # Should Returns: 7:42 AM (9 days later)
+# Should Return: 7:42 AM (9 days later)
